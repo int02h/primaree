a9645c52adff03f7e1935ae1be7162f8110eacf0
Daniil Popov <int02h@gmail.com>
2020-05-13 10:15:11 +0300

    add documentation for all public APIs

--- library/src/main/java/com/dpforge/primaree/ApplicationExtensions.kt
+++ library/src/main/java/com/dpforge/primaree/ApplicationExtensions.kt
@@ -5,11 +5,17 @@ package com.dpforge.primaree
 
 import android.app.Application
 
+/**
+ * Run provided block only if current process is primary process
+ */
 fun Application.runIfPrimaryProcess(block: () -> Unit) {
     if (isPrimaryProcess) {
         block()
     }
 }
 
+/**
+ * @return `true` if current process is primary process, `false` otherwise
+ */
 val Application.isPrimaryProcess: Boolean
     get() = Primaree.currentProcessFullName == packageName
